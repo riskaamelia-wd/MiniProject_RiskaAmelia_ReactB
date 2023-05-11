deafb96f87af705ed36550814c1c143963d9dcd7
riskaamelia-wd <riskaamelia294@gmail.com>
2023-05-11 10:29:35 +0700

    update : package, etc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './templates/Layout';
+import ShopByCategory from './pages/ShopByCategory/ShopByCategory';
+import Detail from './pages/Detail/Detail';
+import { BrowserRouter } from 'react-router-dom';
+import client from './apollo-client';
+import { ApolloProvider } from '@apollo/client';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/Store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Provider store={store}> 
+    //   <PersistGate loading={null} persistor={persistor}>
+        <ApolloProvider client={client}>
+          <BrowserRouter>
+            <Layout/>
+          </BrowserRouter>
+        </ApolloProvider>
+    //   </PersistGate>
+    // </Provider>
   );
 }
 
